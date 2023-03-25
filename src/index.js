dbdbaa7b06948b67f5f121650aa5b59247e6789c
Lisa Riabokon <lisa.checheleva@gmail.com>
2023-03-25 13:04:24 +0200

    create html markup and refs in js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import axios from 'axios';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+// отримуємо доступ до елем зі сторінки
+const refs = {
+  formEl: document.querySelector('.search-form'),
+  inputEl: document.querySelector('input'),
+  gallery: document.querySelector('.gallery'),
+  btnLoadMore: document.querySelector('.load-more'),
+};
+
+// console.log(refs.btnLoadMore);
