fafde7b04566dc9ff41ec12376b8bb4fe076c7dc
Lisa Riabokon <lisa.checheleva@gmail.com>
2023-03-25 13:34:14 +0200

    add event listeners and functions for submit and button load more

--- src/index.js
+++ src/index.js
@@ -11,5 +11,28 @@ const refs = {
   gallery: document.querySelector('.gallery'),
   btnLoadMore: document.querySelector('.load-more'),
 };
-
 // console.log(refs.btnLoadMore);
+
+// слухачі подій для сабміту форми і кнопки завантаж більше
+refs.formEl.addEventListener('submit', onSearchForm);
+refs.btnLoadMore.addEventListener('click', onClickBtnLoadMore);
+// ховаємо кнопку завантаж більше
+refs.btnLoadMore.style.display = 'none';
+
+// функція обробляє подію на submit
+function onSearchForm(e) {
+  // скасовуємо завантаження за замовчуванням
+  e.preventDefault();
+
+  // змінна пейдж =1
+  let page = 1;
+  // очищуємо вміст галереї
+  refs.gallery.innerHTML = '';
+
+  // змінна для пошукового запиту, обрізаємо пробіли до і після
+  const searchQuery = refs.inputEl.value.trim();
+  //   console.log(searchQuery);
+}
+
+// функція обробляє клік на завантаж більше
+function onClickBtnLoadMore() {}
